4dcbeb06ae302ad5d6d32016e9bd777ccad22c6b
Mraz Tevin <millatevin@gmail.com>
2022-02-04 11:25:26 +0300

    styling button using inline styling

--- src/MyButtonComponent.js
+++ src/MyButtonComponent.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const MyButtonComponent = () => {
-    return <button>Sign in</button>
+    return <button
+    style = {{
+        background:"linear-gradient(91.4deg,#2fb8ff 0%,#9eecd9 100%",
+        padding: "12px 0",
+        width: "300px",
+        border: "none",
+        borderRadius:"30px",
+        color:"white",
+        fontWeight: "bold",
+        fontFamily: "SegoeUI, sans-serif"
+    }}>
+    Sign in</button>
 
 }
 
