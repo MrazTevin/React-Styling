8e8e0cf1eec77636555b8d4ed0c57b92f2bd0327
Mraz Tevin <millatevin@gmail.com>
2022-02-04 22:13:02 +0300

    create and style button

--- src/MyButtonComponent.js
+++ src/MyButtonComponent.js
@@ -8,3 +8,20 @@ import styled from "styled-components";
 // }
 
 // export default MyButtonComponent
+
+const MyButton = () => {
+    return <Button>Sign In</Button>
+}
+
+export default MyButton;
+
+const Button = styled.button`
+    background:linear-gradient(91.4deg,#2fb8ff 0%,#9eecd9 100%);
+    padding:12px 0;
+    width: 200px;
+    border: none;
+    border-radius: 30px;
+    color: white;
+    font-weight: bold;
+    font-family: Segoe UI, sans-serif;
+`
