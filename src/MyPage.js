466dc76f554f3c125d00b4a50ae64461c52a9c6d
Mraz Tevin <millatevin@gmail.com>
2022-02-04 11:12:16 +0300

    import and use button

---
+++ src/MyPage.js
@@ -0,0 +1,12 @@
+import MyButtonComponent from "./MyButtonComponent";
+
+const MyPage = () => {
+    return (
+        <div>
+            <h1>Welcome to my application</h1>
+            <MyButtonComponent/>
+        </div>
+    )
+}
+
+export default MyPage
