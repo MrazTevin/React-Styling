c18a99c13c726e0def4abb7c8ce74fce0adb6c02
Mraz Tevin <millatevin@gmail.com>
2022-02-04 11:38:05 +0300

    delete inline style and add classname

--- src/MyButtonComponent.js
+++ src/MyButtonComponent.js
@@ -1,18 +1,7 @@
 import React from 'react';
 
 const MyButtonComponent = () => {
-    return <button
-    style = {{
-        background:"linear-gradient(91.4deg,#2fb8ff 0%,#9eecd9 100%",
-        padding: "12px 0",
-        width: "300px",
-        border: "none",
-        borderRadius:"30px",
-        color:"white",
-        fontWeight: "bold",
-        fontFamily: "SegoeUI, sans-serif"
-    }}>
-    Sign in</button>
+    return <button className="myButton">Sign In</button>
 
 }
 
