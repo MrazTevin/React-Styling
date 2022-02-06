5abbe542b5338185f5e7cc5b40fe2856309c92d2
Mraz Tevin <millatevin@gmail.com>
2022-02-06 18:53:41 +0300

    include card component

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,9 +1,10 @@
 import MyButtonComponent from "./MyButtonComponent";
-
+import MyCard from "./MyCard";
 const MyPage = () => {
     return (
         <div>
             <h1>Welcome to my application</h1>
+            <MyCard/>
             <MyButtonComponent/>
         </div>
     )
