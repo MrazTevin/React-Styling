8272df0958dea72f05294655232a5fd281c6f20d
Mraz Tevin <millatevin@gmail.com>
2022-02-06 18:06:16 +0300

    create card component

---
+++ src/MyCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MyCard = () => {
+    return <Wrapper>The button should be active</Wrapper>
+}
+
+export default MyCard;
+
+const Wrapper  = styled.div `
+    margin-top: 100px;
+    background: rgba(255,255,255,0.6);
+    box-shadow: inset 0 0 0 0.5px rgba(255,255,255,0.6);
+    box-radius: 30px;
+    padding: 20px;
+    width: 300px;
+    display: grid;
+    gap: 30px;
+    justify-items: center;
+    align-items: center;
+    font-family: SegoeUI, sans-serif;
+`;
